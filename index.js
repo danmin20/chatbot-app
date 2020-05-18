9eda74fd7ffd9e6c4b832244f934fd434bf4271a
leejeongmin <danmin20@khu.ac.kr>
2020-05-18 13:59:15 +0900

    dialogflow API

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use("/api/dialogflow", require("./server/routes/dialogflow"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/bulid"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
---
+++ server/routes/dialogflow.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const structjson = require("./structjson.js");
+const dialogflow = require("dialogflow");
+const uuid = require("uuid");
+
+const config = require("../config/dev");
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+
+// Create a new session
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+// Text Query Route
+router.post("/textQuery", async (req, res) => {
+  // send info from client to dialogflow api
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        // The query to send to the dialogflow agent
+        text: req.body.text,
+        // The language used by the client (en-US)
+        languageCode: "en-US",
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+
+  res.send(result);
+});
+
+// Event Query Route
+
+module.exports = router;
