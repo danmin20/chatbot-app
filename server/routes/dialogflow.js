1f2888e638422aad576d5b76b1e8b159d933944d
leejeongmin <danmin20@khu.ac.kr>
2020-05-18 16:13:23 +0900

    event query route

--- server/routes/dialogflow.js
+++ server/routes/dialogflow.js
@@ -40,5 +40,30 @@ router.post("/textQuery", async (req, res) => {
 });
 
 // Event Query Route
+router.post("/eventQuery", async (req, res) => {
+  // send info from client to dialogflow api
+
+  // The text query request.
+  const request = {
+    session: sessionPath,
+    queryInput: {
+      event: {
+        // The query to send to the dialogflow agent
+        name: req.body.event,
+        // The language used by the client (en-US)
+        languageCode: "en-US",
+      },
+    },
+  };
+
+  // Send request and log result
+  const responses = await sessionClient.detectIntent(request);
+  console.log("Detected intent");
+  const result = responses[0].queryResult;
+  console.log(`  Query: ${result.queryText}`);
+  console.log(`  Response: ${result.fulfillmentText}`);
+
+  res.send(result);
+});
 
 module.exports = router;
